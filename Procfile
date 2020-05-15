fc7081a877b6f8e3e74cb63092f2379644abb6ba
Dorian Neto <doriansampaioneto@gmail.com>
2020-05-14 18:26:03 -0700

    change Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: NODE_ENV='production' TZ='utc' pm2 start ./src/server.js
+web: node ./src/server.js
--- src/server.js
+++ src/server.js
@@ -3,6 +3,4 @@ require('dotenv').config();
 const server = require('./app');
 const port = process.env.PORT || 80;
 
-console.log(port);
-
 server.listen(port);
