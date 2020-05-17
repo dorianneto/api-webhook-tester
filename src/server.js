acc511365dca2c95317c0ae99feb62294ef670b6
Dorian Neto <doriansampaioneto@gmail.com>
2020-05-17 11:17:12 -0700

    Revert "add app instance value to server port"
    This reverts commit c91d3dc980408ca5407f7e39188103beec311c68.

--- src/server.js
+++ src/server.js
@@ -2,6 +2,5 @@ require('dotenv').config();
 
 const server = require('./app');
 const port = process.env.PORT || 80;
-const appInstance = process.env.NODE_APP_INSTANCE || 0;
 
-server.listen(port + appInstance);
+server.listen(port);
