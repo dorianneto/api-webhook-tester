c91d3dc980408ca5407f7e39188103beec311c68
Dorian Neto <doriansampaioneto@gmail.com>
2020-05-17 11:04:07 -0700

    add app instance value to server port

--- src/server.js
+++ src/server.js
@@ -2,5 +2,6 @@ require('dotenv').config();
 
 const server = require('./app');
 const port = process.env.PORT || 80;
+const appInstance = process.env.NODE_APP_INSTANCE || 0;
 
-server.listen(port);
+server.listen(port + appInstance);
